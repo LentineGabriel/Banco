2e4941198776340f81e9d5ea57f7f6fa0acd35ba
Gabriel Lentine <gabrielmarinholentine@gmail.com>
2024-08-08 21:23:13 -0300

    Add project files.

---
+++ ContaBancaria.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banco
+{
+    internal class ContaBancaria
+    {
+        public int NumConta { get; private set; } // private set p/ impedir que o número seja alterado
+        public string NomeConta { get; set; }
+        public double Saldo { get; private set; } // mesma coisa aqui
+
+        // conta que não recebe saldo inicial (ou seja, inicializa com zero)
+        public ContaBancaria(int numConta, string nomeConta)
+        {
+            NumConta = numConta;
+            NomeConta = nomeConta;
+        }
+
+        // conta que inicializa com saldo inicial (valor dado pelo usuário)
+        public ContaBancaria(int numConta, string nomeConta, double depositoInicial) : this(numConta, nomeConta)
+        {
+            DepositoConta(depositoInicial);
+        }
+
+        public double DepositoConta(double deposito)
+        {
+            return Saldo = deposito += Saldo;
+        }
+
+        public double SaqueConta(double saque)
+        {
+            return Saldo = (Saldo -= saque) - 5.00;
+        }
+
+        public override string ToString()
+        {
+            return $"Conta {NumConta}, Titular: {NomeConta}, Saldo: {Saldo}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿namespace Banco
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Entre com o número da conta: "); 
+            int numConta = int.Parse(Console.ReadLine());
+
+            Console.Write("Entre com o nome do titular da conta: ");
+            string nomeConta = Console.ReadLine();
+
+            ContaBancaria cb = new ContaBancaria(numConta, nomeConta);
+
+            Console.WriteLine("");
+
+            Console.WriteLine("Deseja realizar um depósito? (s/n)");
+            var option = Console.ReadLine();
+            switch (option)
+            {
+                case "s":
+                    //depósito
+                    Console.Write("Digite o valor do depósito: ");
+                    double dep = double.Parse(Console.ReadLine());
+                    cb.DepositoConta(dep);
+                    Console.WriteLine(cb);
+
+                    //saque
+                    Console.WriteLine("");
+                    Console.WriteLine("Você gostaria de realizar um saque? (s/n)");
+                    var option2 = Console.ReadLine();
+                    switch (option2)
+                    {
+                        case "s":
+                            Console.Write("Digite o valor do saque: ");
+                            double saq = double.Parse(Console.ReadLine());
+                            cb.SaqueConta(saq);
+                            Console.WriteLine(cb);
+                            break;
+
+                        case "n":
+                            Console.WriteLine(cb);
+                            break;
+
+                        default:
+                            Console.WriteLine("Opção inválida");
+                            break;
+                    }
+                    break;
+
+                case "n":
+                    Console.WriteLine(cb);
+                    break;
+
+                default:
+                    Console.WriteLine("Opção inválida");
+                    break;
+            }
+        }
+
+    }
+}
